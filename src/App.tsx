d0dee4834f24106a50281da2c28962a7075e8a2b
Sonia Chacon <srchacon@berkeley.edu>
2025-04-29 15:43:14 -0700

    edited metrics to match figma

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import './App.css'
-import HomePage from './pages/home-page'
-import PerformancePage from './pages/performance-page'
-import RecordPage from './pages/record-page'
-import FeedbackPage from './pages/feedback-page'
-import SpecificFeedbackPage from './pages/specific-feedback-page'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import "./App.css";
+import HomePage from "./pages/home-page";
+import PerformancePage from "./pages/performance-page";
+import RecordPage from "./pages/record-page";
+import FeedbackPage from "./pages/feedback-page";
+import SpecificFeedbackPage from "./pages/specific-feedback-page";
+import MectricsGraphPage from "./pages/metrics-graph-page";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const metric_type = location.state ? location.state.metric_type : "";
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/performance" element={<PerformancePage />} />
-        <Route path="/record" element={<RecordPage />} />
-        <Route path="/feedback" element={<FeedbackPage />} />
-        <Route path="/specific-feedback/:feedback" element={<SpecificFeedbackPage />} />
-        <Route path="/" element={<Navigate to="/home" replace />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <Routes>
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/performance" element={<PerformancePage />} />
+      <Route path="/record" element={<RecordPage />} />
+      <Route path="/feedback" element={<FeedbackPage />} />
+      <Route
+        path="/specific-feedback/:feedback"
+        element={<SpecificFeedbackPage />}
+      />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route
+        path="/metrics-graph"
+        element={<MectricsGraphPage metric_type={metric_type} />}
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/performance-page/metric-card-condensed.tsx
@@ -0,0 +1,72 @@
+import { TrendingUp } from "lucide-react";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+export interface MetricCardProps {
+  title: string;
+  description: string;
+  data: Array<Record<string, any>>;
+  config: ChartConfig;
+  dataKey: string;
+  xAxisKey: string;
+  trendingValue?: string;
+  trendingDirection?: "up" | "down";
+  footerText?: string;
+}
+
+export function MetricCardCondensed({
+  title,
+  description,
+  data,
+  config,
+  dataKey,
+  xAxisKey,
+  trendingValue,
+  trendingDirection = "up",
+  footerText,
+  onClick,
+}: MetricCardProps) {
+  return (
+    <Card className="h-20" onClick={onClick}>
+      <CardContent className="inline-block align-middle text-left flex justify-between">
+        <div className="w-25"> {title} </div>
+        <ChartContainer config={config} className="w-20 h-10">
+          <LineChart accessibilityLayer data={data}>
+            <Line
+              dataKey={dataKey}
+              type="monotone"
+              stroke="var(--color-chart-1)"
+              strokeWidth={2}
+              dot={false}
+              activeDot={{ r: 6, fill: "var(--color-chart-1)" }}
+              isAnimationActive={false}
+            />
+          </LineChart>
+        </ChartContainer>
+        <div> {data[0][dataKey] + 100} </div>
+      </CardContent>
+    </Card>
+  );
+}
